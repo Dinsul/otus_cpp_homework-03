da856b0a85acdede37a0c821b88c36829227ced3
Dinsul <dinsul2.0@gmail.com>
2018-12-17 15:45:22 +0300

    Refactoring
    Renamed OtusList to LinkedList
    Added output std::map wiht std::allocator

--- main.cpp
+++ main.cpp
@@ -1,55 +1,85 @@
 #include "allocator.h"
-#include "otus_list.h"
+#include "linked_list.h"
 
 #include <list>
 #include <map>
 
 int main(int, char *[]) {
-    auto m = std::map<int, unsigned long long int,
-            std::less<unsigned long long int>,
-            otus_allocator<std::pair<const int, unsigned long long int>>>{};
 
-    unsigned long long int factorial = 1;
-
-    for (int i = 0; i < 21; ++i) {
-        m[i] = factorial;
-        factorial *= (i + 1);
-    }
-
-    for (auto i: m) {
-        std::cout << i.first << " " << i.second << std::endl;
-    }
-
-    std::cout << std::endl;
-
-    OtusList<int> list;
-
-    for (int i = 0; i < 10; ++i)
     {
-        list.push_front(i);
+        std::cout << "\n>>> std::map whith standart allocator" << std::endl;
+
+        auto m = std::map<int, unsigned long long int,
+                std::less<unsigned long long int>>{};
+
+        unsigned long long int factorial = 1;
+
+        for (int i = 0; i < 21; ++i) {
+            m[i] = factorial;
+            factorial *= (i + 1);
+        }
+
+        for (auto i: m) {
+            std::cout << i.first << " " << i.second << std::endl;
+        }
+
+        std::cout << std::endl;
     }
 
-    for (auto val : list)
     {
-       std::cout << val << " ";
+        std::cout << "\n>>> std::map whith custom allocator" << std::endl;
+
+        auto m = std::map<int, unsigned long long int,
+                std::less<unsigned long long int>,
+                simple_allocator<std::pair<const int, unsigned long long int>>>{};
+
+        unsigned long long int factorial = 1;
+
+        for (int i = 0; i < 21; ++i) {
+            m[i] = factorial;
+            factorial *= (i + 1);
+        }
+
+        for (auto i: m) {
+            std::cout << i.first << " " << i.second << std::endl;
+        }
+
+        std::cout << std::endl;
     }
 
-    std::cout << std::endl;
-
-    OtusList<int, otus_allocator<int> > list_alloc;
-
-
-    for (int i = 0; i < 10; ++i)
     {
-        list_alloc.push_front(i);
+        std::cout << "\n>>> LinkedList whith standart allocator" << std::endl;
+        LinkedList<int> list;
+
+        for (int i = 0; i < 10; ++i)
+        {
+            list.push_front(i);
+        }
+
+        for (auto val : list)
+        {
+            std::cout << val << " ";
+        }
+
+        std::cout << std::endl;
     }
 
-    for (auto val : list_alloc)
     {
-       std::cout << val << " ";
-    }
+        std::cout << "\n>>> LinkedList whith custom allocator" << std::endl;
+        LinkedList<int, simple_allocator<int> > list_alloc;
 
-    std::cout << std::endl;
+        for (int i = 0; i < 10; ++i)
+        {
+            list_alloc.push_front(i);
+        }
+
+        for (auto val : list_alloc)
+        {
+            std::cout << val << " ";
+        }
+
+        std::cout << std::endl;
+    }
 
     return 0;
 }
